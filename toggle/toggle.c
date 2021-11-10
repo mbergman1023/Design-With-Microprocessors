145784c0a0eaad20faf6743ab6127d462e5fddad
mbergman1023 <56097567+mbergman1023@users.noreply.github.com>
2021-11-10 10:58:28 -0800

    Add files via upload

---
+++ toggle/toggle.c
@@ -0,0 +1,64 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "driverlib/sysctl.h"
+
+#define 	GREEN_MASK 		0x08
+//*****************************************************************************
+//
+//!
+//! A very simple example that toggles the on-board red LED using direct register
+//! access.
+//
+//*****************************************************************************
+
+
+void
+PortFunctionInit(void)
+{
+//
+		volatile uint32_t ui32Loop;   
+	// Enable the GPIO port that is used for the on-board LED.
+    //
+    SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
+
+    //
+    // Do a dummy read to insert a few cycles after enabling the peripheral.
+    //
+    ui32Loop = SYSCTL_RCGC2_R;
+
+    //
+    // Enable the GPIO pin for the red LED (PF1).  Set the direction as output, and
+    // enable the GPIO pin for digital function.
+		//	red = 0x02 blue = 0x04 green = 0x08
+		//	white = 0x0E -> 2+4+8= 14
+    //																	76543210
+		//																	||||||||
+    GPIO_PORTF_DIR_R |= GREEN_MASK; // 0x08 = 00001000 
+    GPIO_PORTF_DEN_R |= GREEN_MASK;
+
+}
+
+
+int main(void)
+{
+	
+		//initialize the GPIO ports	
+		PortFunctionInit();
+	
+    // Turn on the LED.
+    GPIO_PORTF_DATA_R |= GREEN_MASK;
+
+    
+    //
+    // Loop forever.
+    //
+    while(1)
+    {
+        // Delay for a bit.
+				SysCtlDelay(10000000);	// 2000000*5
+
+        // Toggle the LED.
+        GPIO_PORTF_DATA_R ^= GREEN_MASK; // bitwise xor so there's no carry bit
+    }
+}
---
+++ toggle/toggle.d
@@ -0,0 +1,5 @@
+.\toggle.o: toggle.c
+.\toggle.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\toggle.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\toggle.o: ..\..\..\..\inc/tm4c123gh6pm.h
+.\toggle.o: ..\..\..\..\driverlib/sysctl.h
